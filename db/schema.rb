4da1cebc9bd2648ae7f241f6a76963f22aafb66d
Cyberik <cyberik@ya.ru>
2016-09-26 20:13:54 +0300

    route and many-to-many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926063013) do
+ActiveRecord::Schema.define(version: 20160926143820) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -18,10 +18,23 @@ ActiveRecord::Schema.define(version: 20160926063013) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "trains", force: :cascade do |t|
-    t.string   "number"
+  create_table "railway_stations_routes", force: :cascade do |t|
+    t.integer "railway_station_id"
+    t.integer "route_id"
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "trains", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "current_station_id"
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id"
   end
 
 end
