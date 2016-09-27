09b10716f9a0218875e203727e9b23922972ee48
Cyberik <cyberik@ya.ru>
2016-09-27 19:28:33 +0300

    CRUD for routes

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,7 @@
 class Route < ApplicationRecord
+  validates :name, presence: true,
+                     length: { minimum: 5 }
+
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
 end
