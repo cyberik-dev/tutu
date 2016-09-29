e87fbe6e5bf900344d9fc3b82538e35d6db57f7d
Cyberik <cyberik@ya.ru>
2016-09-29 12:02:47 +0300

    add scope to Carriage

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -2,4 +2,8 @@ class Carriage < ApplicationRecord
   belongs_to :train
 
   validates :number, :top_seats, :bottom_seats, presence: true
+
+  scope :economy, -> { where(type: 'EconomyCarriage') }
+  scope :coupe, -> { where(type: 'CoupeCarriage') }
+  scope :ordered, -> { order(:number) }
 end
